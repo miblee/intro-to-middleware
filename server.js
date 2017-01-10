573016ec30ec3f2c49abf724b25bf2d398ae75cb
Michelle Lee <lee.michellejung@gmail.com>
2017-01-09 20:22:26 -0800

    56% sure it's finished

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var fs = require('fs');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
 
 var app = express();
 
+app.use(function(req, res, next){
+  console.log('1. Incoming request. Hello!');
+  req.user = 'Sam';
+  next();
+})
+
+app.use(function(req, res, next){
+  console.log('2.', req.user);
+  next();
+})
+
+app.use(function(req, res, next){
+  console.log('3. I am next. ', req.metho, req.url, req.user);
+  next();
+})
+
+// built in express middleware.
+// if a request URL matches a file in public, serve it.
+// Otherwise call next.
+app.use(morgan('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(express.static(__dirname + '/public'))
+
+
+// if public has an index.html this won't be served.
 app.get('/', function(req, res) {
-  fs.readFile('./index.html', 'utf-8', function(err, data){
-    if (err) throw err;
-    res.set('Content-Type', 'text/html');
-    res.send(data);
-  });
+  res.send("Hello from server.js")
 });
+
+app.get('/about', function(req, res){
+  res.redirect('/bio.html')
+})
+
 
 var port = 3000;
 app.listen(port, function(){
